ba2bea8dae979b7ba828e7c14c483f8b0a1f7abe
Alan Plotko <alan.plotko@gmail.com>
2015-06-14 12:22:20 -0400

    Fix and merge files

--- app/main.js
+++ app/main.js
@@ -31,7 +31,7 @@ app.on('ready', function () {
 
     if (env.name === 'development') {
         devHelper.setDevMenu();
-        //mainWindow.openDevTools();
+        mainWindow.openDevTools();
     }
 
     mainWindow.on('close', function () {
